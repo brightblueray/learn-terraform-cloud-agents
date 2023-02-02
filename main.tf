fa21f35834d5f221c21ef588c00ee0862ac7cf01
Ray Ryjewski <brightblueray@users.noreply.github.com>
2023-02-02 15:03:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx
   name  = "nginx"
   ports {
     internal = 80
